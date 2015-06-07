c4a65b34b87795e82c28d9ad0fb853098f9cebaa
Thomas Belin <thomasbelin4@gmail.com>
2015-06-07 15:49:21 +0200

    use env variables to connect to database

--- index.js
+++ index.js
@@ -4,7 +4,12 @@
 var app = require("./app"),
     db = require("./db/db");
 
-db.init({});
+db.init({
+    host:     process.env["POSTGRESQL_ADDON_HOST"] || "localhost",
+    user:     process.env["POSTGRESQL_ADDON_USER"] || "",
+    password: process.env["POSTGRESQL_ADDON_PASSWORD"] || "",
+    db:       process.env["POSTGRESQL_ADDON_DB"] || "historyof"
+});
 
 app.listen(process.env.PORT || 1337, function () {
     console.log("listening at %s", process.env.PORT || 1337);

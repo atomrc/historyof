123818ebb1bb7e0b151c93648750b2eb6dcca315
Thomas Belin <thomasbelin4@gmail.com>
2015-10-18 21:27:26 +0200

    transform the Timeline component into a simple function

--- src/js/components/Timeline.react.js
+++ src/js/components/Timeline.react.js
@@ -11,59 +11,55 @@
      *
      * @return {undefined}
      */
-    var Timeline = React.createClass({
+    var Timeline = function({user, events}) {
+        var yearNodes = [],
+            groupedEvents = events
+                .sort(function (e1, e2) {
+                    if (e1.date === e2.date) { return 0; }
+                    return e1.date > e2.date ? -1 : 1;
+                })
+                .reduce(function (obj, e) {
+                    var key = e.date.getFullYear().toString();
+                    if (!obj[key]) { obj[key] = []; }
+                    obj[key].push(e);
+                    return obj;
+                }, {});
 
-        render: function() {
-            var yearNodes = [],
-                user = this.props.user,
-                groupedEvents = this
-                    .props
-                    .events
-                    .sort(function (e1, e2) {
-                        if (e1.date === e2.date) { return 0; }
-                        return e1.date > e2.date ? -1 : 1;
-                    })
-                    .reduce(function (obj, e) {
-                        var key = e.date.getFullYear().toString();
-                        if (!obj[key]) { obj[key] = []; }
-                        obj[key].push(e);
-                        return obj;
-                    }, {});
+        for (var i in groupedEvents) {
+            yearNodes.push((
+                <Year
+                    key={"year-" + i}
+                    year={i}
+                    events={groupedEvents[i]}/>
+            ));
+        }
 
-            for (var i in groupedEvents) {
-                yearNodes.push((
-                    <Year
-                        key={"year-" + i}
-                        year={i}
-                        events={groupedEvents[i]}/>
-                ));
-            }
+        var content = yearNodes.length === 0 ?
+            (<div>No event yet</div>) :
+            (<div className="events-container fluid-content">{yearNodes}</div>);
 
-            var content = yearNodes.length === 0 ?
-                (<div>No event yet</div>) :
-                (<div className="events-container fluid-content">{yearNodes}</div>);
-
-            return (
-                <div className="timeline">
-                    <div className="timeline-header">
-                        <table className="fluid-content">
+        return (
+            <div className="timeline">
+                <div className="timeline-header">
+                    <table className="fluid-content">
+                        <tbody>
                             <tr>
                                 <td>
                                 <h1>{user.pseudo}'s timeline</h1>
-                                <span>{this.props.events.length} events</span>
+                                <span>{events.length} events</span>
                                 </td>
                                 <td>
                                 <WriteButton/>
                                 </td>
                             </tr>
-                        </table>
-                    </div>
-                    {content}
-                    <EventForm/>
+                        </tbody>
+                    </table>
                 </div>
-            );
-        }
-    });
+                {content}
+                <EventForm/>
+            </div>
+        );
+    };
 
     module.exports = Timeline;
 }());

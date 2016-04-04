ee3457c4cf6f0ace0a5cff3bb0c9bc6669ae2fc8
Thomas Belin <thomasbelin4@gmail.com>
2016-04-04 19:30:54 +0200

    add the app component

--- src/js/application.js
+++ src/js/application.js
@@ -1,29 +1,15 @@
 import {run} from '@cycle/core';
 import {makeDOMDriver} from '@cycle/dom';
-import isolate from '@cycle/isolate';
-import Timeline from "./components/Timeline";
-import assign from "object-assign";
 import apiDriver from "./apiDriver";
+import App from "./components/App";
 
 function main({DOM, api}) {
-    const stories$ = api
-        .filter(request => request.action.type === "fetchStories")
-        .map(({ response }) => response);
-
-    const timeline = isolate(Timeline)({DOM, stories$: stories$});
+    const app = App({DOM, api, token: "okman"});
 
     return {
-        DOM: timeline.DOM,
-        api: timeline
-            .action$
-            .map((action) =>
-                //décorate the action that is given to the api
-                //with the user's token that authenticate him against the
-                //server
-                assign({}, action, { token: "felix" })
-            )
-            .startWith({ type: "fetchStories" })
-    };
+        DOM: app.DOM,
+        api: app.api
+    }
 }
 
 var drivers = {
---
+++ src/js/components/App.js
@@ -0,0 +1,27 @@
+import isolate from '@cycle/isolate';
+import Timeline from "./Timeline";
+import assign from "object-assign";
+
+function App({DOM, api, token}) {
+
+    const stories$ = api
+        .filter(request => request.action.type === "fetchStories")
+        .map(({ response }) => response);
+
+    const timeline = isolate(Timeline)({DOM, stories$: stories$});
+
+    return {
+        DOM: timeline.DOM,
+        api: timeline
+            .action$
+            .map((action) =>
+                //décorate the action that is given to the api
+                //with the user's token that authenticate him against the
+                //server
+                assign({}, action, { token: "felix" })
+            )
+            .startWith({ type: "fetchStories" })
+    }
+}
+
+export default App;

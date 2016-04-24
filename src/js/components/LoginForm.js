e2620538c7e8b4096a9549f62996b59fee95d984
Thomas Belin <thomasbelin4@gmail.com>
2016-04-24 18:31:41 +0200

    add the login form component

---
+++ src/js/components/LoginForm.js
@@ -0,0 +1,74 @@
+import {Observable} from "rx";
+import {form, input, div} from "@cycle/dom";
+import assign from "object-assign";
+
+function intent(DOM, api) {
+    const login$ = DOM
+        .select("input[name=login]")
+        .events("keyup")
+        .map(ev => ev.target.value);
+
+    const password$ = DOM
+        .select("input[name=password]")
+        .events("keyup")
+        .map(ev => ev.target.value);
+
+    const loginValues$ = Observable
+        .combineLatest(login$, password$, (login, password) => ({ login, password }))
+
+    const loginRequest$ = DOM
+        .select("form")
+        .events("submit")
+        .map(ev => { ev.preventDefault(); return ev; });
+
+    const loginResponse$ = api
+        .filter(req => req.action.type === "login")
+        .flatMap(req => req.response);
+
+    const loginError$ = loginResponse$
+        .catch(error => Observable.just(error));
+
+    return {
+        loginRequest$: loginValues$.sample(loginRequest$),
+        loginSuccess$: loginResponse$,
+        loginError$
+    };
+}
+
+function view(loginError$) {
+    return loginError$
+        .startWith(null)
+        .map(error => {
+            var inputs = [
+                input({type: "text", name: "login"}),
+                input({type: "password", name: "password"}),
+                input({type: "submit", name: "Go!"})
+            ]
+            if (error) {
+                inputs = [div(".error", error.error)].concat(inputs);
+            }
+            return form(inputs);
+        })
+}
+
+function LoginForm({DOM, api}) {
+    const { loginRequest$, loginSuccess$, loginError$ } = intent(DOM, api);
+
+    const user$ = loginSuccess$
+        .map(res => res.user);
+
+    const token$ = loginSuccess$
+        .map(res => res.token);
+
+    const apiLoginRequest$ = loginRequest$
+        .map(loginValues => assign({}, { type: "login" }, loginValues));
+
+    return {
+        DOM: view(loginError$),
+        api: apiLoginRequest$,
+        user$,
+        token$
+    }
+}
+
+export default LoginForm;
---
+++ tests/front/components/LoginForm.js
@@ -0,0 +1,91 @@
+/*global __dirname, it, describe, require*/
+"use strict";
+const APP_PATH = __dirname + "/../../../src/js";
+
+import expect from "expect.js";
+import {mockDOMSource} from '@cycle/dom';
+
+import {Observable} from "rx";
+
+describe("LoginForm Component", () => {
+    const LoginForm = require(APP_PATH + "/components/LoginForm").default;
+
+    it("should display login form", (done) => {
+        const DOM = mockDOMSource();
+        const sinks = LoginForm({ DOM, api: Observable.empty() });
+
+        sinks.DOM.subscribe(vtree => {
+            expect(vtree.tagName).to.be("FORM");
+        });
+
+        sinks
+            .api
+            .isEmpty()
+            .subscribe(isEmpty => {
+                expect(isEmpty).to.be(true);
+                done();
+            });
+    });
+
+    it("should send login request when user logs in", (done) => {
+        const DOM = mockDOMSource({
+                "input[name=login]": { keyup: Observable.just({
+                    target: { value: "felix@felix.fr" }
+                }) },
+                "input[name=password]": { keyup: Observable.just({
+                    target: { value: "password" }
+                }) },
+                "form": { submit: Observable.just({ preventDefault: () => 1}) }
+            });
+
+        const sinks = LoginForm({ DOM, api: Observable.empty() });
+
+        sinks.api.subscribe(request => {
+            expect(request.type).to.be("login");
+            done();
+        });
+
+    });
+
+    it("should return user and token logged in", (done) => {
+        const DOM = mockDOMSource(),
+            loginResponse$ = Observable.just({
+                user: {
+                    pseudo: "felix", login: "felix@felix.fr", password: "password"
+                },
+                token: "usertoken"
+            }),
+            apiResponse$ = Observable.just({ action: { type: "login" }, response: loginResponse$ });
+
+        const sinks = LoginForm({ DOM, api: apiResponse$ });
+
+        sinks.user$.subscribe(user => {
+            expect(user.pseudo).to.be("felix");
+            expect(user.login).to.be("felix@felix.fr");
+        });
+
+        sinks.token$.subscribe(token => {
+            expect(token).to.be("usertoken");
+            done();
+        });
+    });
+
+    it("should display error if login fails", (done) => {
+        const DOM = mockDOMSource(),
+            loginResponse$ = Observable.fromPromise(new Promise(function (resolve, reject) {
+                reject({ error: "login/password don't match" });
+            })),
+            apiResponse$ = Observable.just({ action: { type: "login" }, response: loginResponse$ });
+
+        const sinks = LoginForm({ DOM, api: apiResponse$ });
+
+        sinks
+            .DOM
+            .last()
+            .subscribe(vtree => {
+                expect(vtree.children[0].properties.className).to.be("error");
+                done();
+            });
+    });
+
+});

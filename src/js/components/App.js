a6cc8f0b4f9c6136117f7850737693c2ab37fca6
Thomas Belin <thomasbelin4@gmail.com>
2016-04-30 13:32:44 +0200

    Fetch stories on App component init

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,29 +2,47 @@ import {Observable} from "rx";
 import {div, button} from "@cycle/dom";
 
 
-function intent(DOM) {
+function intent(DOM, api) {
     const logoutAction$ = DOM
         .select(".logout")
         .events("click")
         .map({ type: "logout" });
 
-    return logoutAction$;
+
+    const storiesResponse$ = api
+        .filter(req => req.action.type === "fetchStories")
+        .flatMap(({ response$ }) => {
+            return response$
+                .catch(error => Observable.just({ error }));
+        });
+
+    return {
+        logoutAction$,
+        storiesResponse$
+    };
 }
 
-function view(user$) {
-    return user$
-        .map(user => div([
+function view(user$, stories$) {
+    return Observable.combineLatest(
+            user$,
+            stories$.startWith(null),
+            (user, stories) => ({user, stories})
+        )
+        .map(({user, stories}) => div([
             user.pseudo,
-            button(".logout", "Logout")
+            button(".logout", "Logout"),
+            (stories ? stories.length : "loading")
         ]));
 }
 
 function App({DOM, api, user$}) {
-    const logoutAction$ = intent(DOM);
+    const { logoutAction$, storiesResponse$ } = intent(DOM, api);
+
+    const apiRequest$ = Observable.just({ type: "fetchStories" });
 
     return {
-        DOM: view(user$),
-        api: Observable.empty(),
+        DOM: view(user$, storiesResponse$),
+        api: apiRequest$,
         logoutAction$
     }
 }

ca2078422b9446fe22a89dd9c9a46e792198dd02
Thomas Belin <thomasbelin4@gmail.com>
2016-07-12 22:28:24 +0200

    Complete the auth component wrapper

--- src/js/authentication/componentWrapper.js
+++ src/js/authentication/componentWrapper.js
@@ -1,4 +1,5 @@
 import xs from "xstream";
+import {div} from "@cycle/dom";
 import jwtDecode from "jwt-decode";
 
 
@@ -8,30 +9,39 @@ function model(storage, router) {
         .getItem("token")
 
     const user$ = token$
-        .filter(token => !!token)
-        .map(jwtDecode)
-        .startWith(null);
+        .map(token => token ? jwtDecode(token) : null);
 
-    const hashInfo$ = router
+    const hasHashToken$ = router
         .history$
-        .map(({ hash }) => ({ hash, hasToken: hash.indexOf("id_token") > -1 }));
+        .map(location => location.hash.indexOf("id_token") > -1);
 
-    return xs
-        .combine(token$, user$, hashInfo$)
-        .map(([ token, user, hashInfo ]) => ({ token, user, hashInfo }))
-        .remember();
+    return {
+        user$: user$.remember(),
+        state$: xs
+            .combine(token$, router.history$, hasHashToken$)
+            .map(([ token, location, hasHashToken ]) => ({ token, location, hasHashToken }))
+            .remember()
+    };
+}
+
+function render(user$, componentDOM) {
+    return user$
+        .map(user => {
+            return user ?
+                componentDOM :
+                xs.of(div(".unlogged"))
+        })
+        .flatten();
 }
 
 function wrap(Component) {
     return function protectedInstanciation(sources) {
         const { storage, router, auth0 } = sources;
 
-        const state$ = model(storage, router);
-        const user$ = state$
-            .map(({ user }) => user)
-            .filter(user => !!user);
+        const { user$, state$ } = model(storage, router);
+        const loggedUser$ = user$.filter(user => !!user).remember();
 
-        const sinks = Component(Object.assign({}, sources, { props: { user$: user$ }}));
+        const sinks = Component(Object.assign({}, sources, { props: { user$: loggedUser$ }}));
 
         const tokenSaveRequest$ = auth0
             .filter(({ action }) => action.action === "parseHash")
@@ -40,26 +50,36 @@ function wrap(Component) {
             .map(response => ({ key: "token", value: response }));
 
         const showLoginRequest$ = state$
-            .filter(({ token, hashInfo }) => !token && !hashInfo.hasToken)
+            .filter(({ token, hasHashToken }) => !token && !hasHashToken)
             .mapTo({ action: "show", params: {
                 authParams: { scope: "openid nickname" },
                 responseType: "token"
             }});
 
         const parseHashRequest$ = state$
-            .filter(({ token, hashInfo }) => !token && hashInfo.hasToken)
-            .map(({ hashInfo }) => ({ action: "parseHash", params: hashInfo.hash }));
+            .filter(({ token, hasHashToken }) => !token && hasHashToken)
+            .map(({ location }) => ({ action: "parseHash", params: location.hash }));
 
         const tokenRemoveRequest$ = (sinks.action$ || xs.empty())
             .filter(action => action.type === "logout")
             .mapTo({ action: "removeItem", key: "token" })
 
+        const cleanHash$ = xs
+            .combine(state$, loggedUser$)
+            .map(([ state ]) => ({ location: state.location, hasHashToken: state.hasHashToken }))
+            .filter(({ hasHashToken }) => hasHashToken)
+            .map(({ location }) => location.pathname);
+
         return Object.assign({}, sinks, {
+            DOM: render(user$, sinks.DOM),
+
             storage: xs.merge(
                 tokenRemoveRequest$,
                 tokenSaveRequest$,
                 sinks.storage || xs.empty()
             ),
+
+            router: xs.merge(cleanHash$, sinks.router || xs.empty()),
 
             auth0: xs.merge(showLoginRequest$, parseHashRequest$, sinks.auth0 || xs.empty()),
             //decorate all the component api requests with
--- tests/front/authentication/componentWrapper.js
+++ tests/front/authentication/componentWrapper.js
@@ -3,6 +3,8 @@
 const APP_PATH = __dirname + "/../../../src/js";
 
 import expect from "expect.js";
+import select from "snabbdom-selector";
+import jwt from "jsonwebtoken";
 
 import xs from "xstream";
 
@@ -13,7 +15,8 @@ const emptyListener = {
 };
 
 describe("Authentication Component wrapper", () => {
-    const wrap = require(APP_PATH + "/authentication/componentWrapper").default;
+    const wrap = require(APP_PATH + "/authentication/componentWrapper").default,
+        token = jwt.sign({ nickname: "felix" }, "secret");
 
     function getDefaultSources() {
         return {
@@ -27,15 +30,26 @@ describe("Authentication Component wrapper", () => {
         return {};
     }
 
-    it("should display Auth0 login form", (done) => {
-        const { auth0 } = wrap(DummyComponent)(getDefaultSources());
+    describe("Unlogged", function () {
+        const { auth0, DOM } = wrap(DummyComponent)(getDefaultSources());
 
-        auth0.addListener(Object.assign({}, emptyListener, {
-            next: action => {
-                expect(action.action).to.be("show");
-                done();
-            }
-        }));
+        it("should trigger Auth0 login form", (done) => {
+            auth0.addListener(Object.assign({}, emptyListener, {
+                next: action => {
+                    expect(action.action).to.be("show");
+                    done();
+                }
+            }));
+        });
+
+        it("should display unlogged layout", (done) => {
+            DOM.addListener(Object.assign({}, emptyListener, {
+                next: vtree => {
+                    expect(select(".unlogged", vtree).length).to.be(1);
+                    done();
+                }
+            }));
+        });
     });
 
     it("should parse token when user is logged in", (done) => {
@@ -78,10 +92,52 @@ describe("Authentication Component wrapper", () => {
             }));
     });
 
-    it("should display component when user is logged in", (done) => {
+    describe("Login success", function () {
         function Component() {
             return {
                 DOM: xs.of("component DOM")
+            };
+        }
+
+        const sources = Object.assign({}, getDefaultSources(), {
+            router: { history$: xs.of({ pathname: "/felix", hash: "#id_token=b64token" }) },
+            storage: {
+                local: {
+                    getItem: () => xs.of(token)
+                }
+            }
+        });
+
+        const {DOM, router} = wrap(Component)(sources);
+
+        it("should display component when user is logged in", (done) => {
+            DOM
+                .take(1)
+                .addListener(Object.assign({}, emptyListener, {
+                    next: dom => {
+                        expect(dom).to.be("component DOM");
+                        done();
+                    }
+                }));
+        });
+
+        it("should remove id_token from location hash", (done) => {
+            router
+                .take(1)
+                .addListener(Object.assign({}, emptyListener, {
+                    next: path => {
+                        expect(path).to.be("/felix");
+                        done();
+                    }
+                }));
+        });
+    });
+
+
+    it("should remove token when component outputs a logout", (done) => {
+        function Component() {
+            return {
+                action$: xs.of({ type: "logout" })
             };
         }
 
@@ -93,16 +149,16 @@ describe("Authentication Component wrapper", () => {
             }
         });
 
-        const {DOM} = wrap(Component)(sources);
+        const { storage } = wrap(Component)(sources);
 
-        DOM
+        storage
             .take(1)
             .addListener(Object.assign({}, emptyListener, {
-                next: dom => {
-                    expect(dom).to.be("component DOM");
+                next: store => {
+                    expect(store.action).to.be("removeItem");
+                    expect(store.key).to.be("token");
                     done();
                 }
             }));
     });
-
 });
